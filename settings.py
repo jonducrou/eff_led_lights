325b7d99d6c96cb7515a265851c3fd897567b872
Jon <jonducrou@gmail.com>
2019-02-01 18:58:02 +1000

    fix style

--- settings.py
+++ settings.py
@@ -9,34 +9,28 @@ import os.path
 
 _DATA = {}
 
+
 def getDataFile(domain):
-  return "./cfg/" + domain + ".json"
+    return "./cfg/" + domain + ".json"
+
 
 def set(domain, key, value):
-  global _DATA
-  if not domain in _DATA:
-    _DATA[domain] = {}
-  _DATA[domain][key] = value
-  with open(getDataFile(domain),"w") as out:
-    json.dump(_DATA[domain], out)
-
+    global _DATA
+    if domain not in _DATA:
+        _DATA[domain] = {}
+    _DATA[domain][key] = value
+    with open(getDataFile(domain), "w") as out:
+        json.dump(_DATA[domain], out)
 
 
 def get(domain, key):
-  global _DATA
-  if not domain in _DATA:
-    if os.path.isfile(getDataFile(domain)):
-      with open(getDataFile(domain)) as infile:
-        _DATA[domain] = json.load(infile)
-    else:
-      return None
-  if not key in _DATA[domain]:
-    return None
-  return _DATA[domain][key]
-  
-
-
-
-
-
-
+    global _DATA
+    if domain not in _DATA:
+        if os.path.isfile(getDataFile(domain)):
+            with open(getDataFile(domain)) as infile:
+                _DATA[domain] = json.load(infile)
+        else:
+            return None
+        if key not in _DATA[domain]:
+            return None
+        return _DATA[domain][key]
