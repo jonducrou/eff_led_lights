111d7b439849f1f4012d8851c1e6b7fe0a1d0600
Jon <jonducrou@gmail.com>
2019-02-04 21:56:47 +1000

    add server methods

--- vis/gridder.py
+++ vis/gridder.py
@@ -1,21 +1,34 @@
+POS = 0
 
 
+def getDefaultParams():
+    return {"grid_size": 40, "colour": (100, 100, 100)}
 
-POS=0
+
+def customParams():
+    return [
+        {"name": "grid_size", "type": "number"},
+        {"name": "colour", "type": "rgb"},
+        ]
+
 
 def go(params, h, w, led_control):
     global POS
     POS = POS+1
     grid_size = params["grid_size"]
+    r = params["colour"][0]
+    g = params["colour"][1]
+    b = params["colour"][2]
+
     for x in range(w):
         for y in range(h):
-            led_control.setLEDs(x,y,0,0,0)
+            led_control.setLEDs(x, y, 0, 0, 0)
             if ((POS+x) % grid_size == 0):
-                led_control.setLEDs(x,y,100,100,100)
+                led_control.setLEDs(x, y, r, g, b)
             if ((POS+y) % grid_size == 0):
-                led_control.setLEDs(x,y,100,100,100)
+                led_control.setLEDs(x, y, r, g, b)
 
             if ((POS+x) % grid_size == 1 or (POS+x) % grid_size == grid_size - 1):
-                led_control.setLEDs(x,y,30,0,30)
+                led_control.setLEDs(x, y, r/3, g/3, b/3)
             if ((POS+y) % grid_size == 1 or (POS+y) % grid_size == grid_size - 1):
-                led_control.setLEDs(x,y,30,30,0)
+                led_control.setLEDs(x, y, r/3, g/3, b/3)
